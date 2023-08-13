2167646f196526ef128017e68a4aac86c4d149f2
Serena <serenagaia.scicolone@gmail.com>
2023-08-13 16:16:16 +0200

    add textarea and update css

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,13 +2,16 @@ import { useDispatch } from 'react-redux';
 import { deleteTask, toggleTask } from "../features/todo/taskSlice";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { CiCircleCheck } from "react-icons/ci";
-
+import { useState } from 'react';
 
 
 function Task ({task}) {
 const dispatch = useDispatch();
-    return (
-        <article className="task-container">
+const [isEditing, setIsEditing] = useState(true)
+
+let taskView = (
+ 
+       <>
             <span onClick={()=> dispatch(toggleTask(task.id))}><CiCircleCheck className={`task-button toggle ${task.completed ? 'checked' : ''}`} style= {{borderRadius : "50%"}}/></span>
         {/* <input type="checkbox" checked={task.completed}  onChange={()=> dispatch(toggleTask(task.id))}/> */}
             <span onClick = {()=>dispatch(deleteTask(task.id))}> <AiFillCloseCircle className="task-button"  style= {{borderRadius : "50%"}} /> </span>
@@ -16,7 +19,30 @@ const dispatch = useDispatch();
         <div className="buttons-container">
             <button>Modifica</button>
         </div>
-    </article>  
+       </>
+
+    
+    )
+
+let taskEdit = (
+    <form action="">
+        <span onClick={()=> dispatch(toggleTask(task.id))}><CiCircleCheck className={`task-button toggle ${task.completed ? 'checked' : ''}`} style= {{borderRadius : "50%"}}/></span>
+    
+        <span onClick = {()=>dispatch(deleteTask(task.id))}> <AiFillCloseCircle className="task-button"  style= {{borderRadius : "50%"}} /> </span>
+    {/* <p className={task.completed ? 'completed' : ''}> {task.name}</p> */}
+        {/* <input type="text" /> */}
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <div className="buttons-container">
+            <button>Salva</button>
+        </div>
+    </form>
+    )
+
+
+    return (
+        <article className="task-container">
+            {isEditing ? taskEdit : taskView}
+        </article>
     )
 }
 
